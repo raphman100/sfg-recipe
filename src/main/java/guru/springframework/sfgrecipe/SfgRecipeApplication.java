cd6bc4b902ea949efaaffb11dad396405cd9d3ae
Unknown <raphman100@gmail.com>
2020-04-11 16:31:21 -0400

    Initial Setup

---
+++ src/main/java/guru/springframework/sfgrecipe/SfgRecipeApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgrecipe/SfgRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
