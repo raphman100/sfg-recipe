be17405e52b1b7dba4281eeb4a4f0c5486e5a564
Unknown <raphman100@gmail.com>
2020-04-19 16:49:33 -0400

    Added bootstrap for data recipe data and updated other classes

--- src/main/java/guru/springframework/sfgrecipe/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgrecipe/controllers/IndexController.java
@@ -1,34 +1,22 @@
 package guru.springframework.sfgrecipe.controllers;
 
-import guru.springframework.sfgrecipe.domain.Category;
-import guru.springframework.sfgrecipe.domain.UnitOfMeasure;
-import guru.springframework.sfgrecipe.repositories.CategoryRepository;
-import guru.springframework.sfgrecipe.repositories.UnitOfMeasureRepository;
+import guru.springframework.sfgrecipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-	private CategoryRepository categoryRepository;
-	private UnitOfMeasureRepository unitOfMeasureRepository;
+	private final RecipeService recipeService;
 
-	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-		this.categoryRepository = categoryRepository;
-		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	public IndexController(RecipeService recipeService) {
+		this.recipeService = recipeService;
 	}
 
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String getIndexPage() {
-
-		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-		Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Tablespoon");
-
-		System.out.println("Category: " + categoryOptional.get().getDescription());
-		System.out.println("UnitOfMeasure: " + unitOfMeasure.get().getDescription());
-
+	@RequestMapping({"", "/", "/index"})
+	public String getIndexPage(Model model) {
+		model.addAttribute("recipes", recipeService.getRecipes());
 		return "index";
 	}
 }
--- src/main/java/guru/springframework/sfgrecipe/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/sfgrecipe/repositories/RecipeRepository.java
@@ -4,5 +4,4 @@ import guru.springframework.sfgrecipe.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
 }
