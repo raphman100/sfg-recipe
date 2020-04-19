a704a5e1dab011c30bdc048d02d4eda9e6e3b8dd
Unknown <raphman100@gmail.com>
2020-04-18 21:49:41 -0400

    Update repositories to get data from db

--- src/main/java/guru/springframework/sfgrecipe/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgrecipe/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.sfgrecipe.controllers;
 
+import guru.springframework.sfgrecipe.domain.Category;
+import guru.springframework.sfgrecipe.domain.UnitOfMeasure;
+import guru.springframework.sfgrecipe.repositories.CategoryRepository;
+import guru.springframework.sfgrecipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String getIndexPage() {
+
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+		System.out.println("Category: " + categoryOptional.get().getDescription());
+		System.out.println("UnitOfMeasure: " + unitOfMeasure.get().getDescription());
+
 		return "index";
 	}
 }
--- src/main/java/guru/springframework/sfgrecipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/sfgrecipe/repositories/UnitOfMeasureRepository.java
@@ -3,6 +3,9 @@ package guru.springframework.sfgrecipe.repositories;
 import guru.springframework.sfgrecipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+	Optional<UnitOfMeasure> findByDescription(String description);
 }
