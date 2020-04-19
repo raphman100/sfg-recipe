da1b8b61fc3d75eb8ec2bba809c1d43d2962c020
Unknown <raphman100@gmail.com>
2020-04-18 20:41:44 -0400

    Added Category with ManyToMany with Recipe

--- src/main/java/guru/springframework/sfgrecipe/domain/Recipe.java
+++ src/main/java/guru/springframework/sfgrecipe/domain/Recipe.java
@@ -30,6 +30,12 @@ public class Recipe {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+		joinColumns = @JoinColumn(name = "recipe_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
+
 	public Long getId() {
 		return id;
 	}
@@ -126,4 +132,11 @@ public class Recipe {
 		this.difficulty = difficulty;
 	}
 
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
 }
