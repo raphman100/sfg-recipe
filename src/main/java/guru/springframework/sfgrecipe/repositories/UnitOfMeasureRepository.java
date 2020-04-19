c7ea9b159eba9bcfdaa19701bf2428475ec804a8
Unknown <raphman100@gmail.com>
2020-04-18 21:07:52 -0400

    Added 3 repository files

---
+++ src/main/java/guru/springframework/sfgrecipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgrecipe.repositories;
+
+import guru.springframework.sfgrecipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
